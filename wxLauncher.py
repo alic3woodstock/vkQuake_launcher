cffc469c5f35b7b6847bca3b111ce85d8a2ed4e7
alic3woodstock <alic3woodstock@gmail.com>
2022-10-06 11:26:02 -0300

    Hadle events and run game

--- wxLauncher.py
+++ wxLauncher.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import wx
+import os
+
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         self.itens = []
@@ -9,9 +11,12 @@ class MyFrame(wx.Frame):
         self.panel = wx.Panel(self, wx.ID_ANY)
         btnOk = wx.Button(self.panel, wx.ID_ANY, 'OK')
         btnCancel = wx.Button(self.panel, wx.ID_ANY, 'Cancelar')
+        self.listCtrl = wx.ListCtrl(self.panel, id=wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_NO_HEADER)
+
+        #Bind Events
         self.Bind(wx.EVT_BUTTON, self.btnCancelOnPress, btnCancel)
         self.Bind(wx.EVT_BUTTON, self.btnOkOnPress, btnOk)
-        self.listCtrl = wx.ListCtrl(self.panel, id=wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_NO_HEADER)
+        self.listCtrl.Bind(wx.EVT_KEY_DOWN, self.listCtrlOnKeyDown)
 
         box = wx.BoxSizer(wx.VERTICAL)
         box.Add(self.listCtrl, 1, wx.EXPAND | wx.ALL,0)
@@ -27,7 +32,11 @@ class MyFrame(wx.Frame):
         self.listCtrl.AppendColumn('Levels', width=self.listCtrl.GetSize().GetWidth())
         itens = self.itens
         itens.append(GameDef(id = 0, str = "Quake - First Episodes", dir = "id1"))
-        itens.append(GameDef(id = 1, str = "Scourge of Armagon (mission pack 1)", dir = "hipnotic"))
+        itens.append(GameDef(id = 1, str = "Scourge of Armagon", dir = "hipnotic"))
+        itens.append(GameDef(id = 2, str = "Dissolution of Eternity", dir = "rogue"))
+        itens.append(GameDef(id = 3, str = "Dimension of the Pastn", dir = "dopa"))
+        itens.append(GameDef(id = 4, str = "Dimension of the Machine", dir = "mg1"))
+        itens.append(GameDef(id = 5, str = "Arcane Dimensions", dir = "ad"))
 
         for i in range(len(itens)):
             self.listCtrl.InsertItem(itens[i].GetItem())
@@ -42,8 +51,21 @@ class MyFrame(wx.Frame):
         self.Close()
 
     def btnOkOnPress(self, event):
+        self.lauchGame()
+
+    def listCtrlOnKeyDown(self, event):
+        event.DoAllowNextEvent()
+        if (event.GetKeyCode() == 13):
+            self.lauchGame()
+        else:
+            event.Skip()
+
+    def lauchGame(self):
         item = self.itens[self.listCtrl.GetFocusedItem()]
-        print(item.GetItem().GetText())
+        os.popen("./vkquake -game " + item.GetGameDir())
+        self.listCtrl.SetFocus()
+
+
 
 
 class GameDef():
